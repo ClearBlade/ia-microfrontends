2bfcce89af46ed99626bb325008a8cdbb229c16a
Rajas Deshpande <rajasdeshpande27@gmail.com>
2024-10-23 12:21:06 -0500

    fixed merge conflicts

---
+++ src/aiComponents/types.ts
@@ -0,0 +1,14 @@
+import { AssetType } from "@clearblade/ia-mfe-core";
+
+export type AiComponentsProps = {
+  schema: Record<string, unknown>[];
+  componentMeta: Record<string, unknown>;
+  componentName: string;
+  assetTypeName: string;
+  setValues: React.Dispatch<
+    React.SetStateAction<{
+      schema: Record<string, unknown>[];
+      componentsMeta: Record<string, unknown>;
+    }>
+  >
+};
